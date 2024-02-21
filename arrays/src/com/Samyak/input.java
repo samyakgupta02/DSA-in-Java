e144b415b68f0f01aa1c568f4244130cb941a20b
Samyak Gupta <samyakgupta02@gmail.com>
2024-02-21 16:48:00 +0530

    Initial commit Room 103

---
+++ arrays/src/com/Samyak/input.java
@@ -0,0 +1,34 @@
+//package com.Samyak;
+//import java.util.Scanner;
+//
+//public class input {
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//
+//        //this is an array of primitives
+//        int[] arr = new int[5];
+//        arr[0] = 23;
+//        arr[1] = 45;
+//        arr[2] = 44;
+//        arr[3] = 46;
+//        arr[4] = 47;
+//
+//        System.out.println(arr[3]);
+//
+//        //this is repetitive. so we will do this by,
+////        // Using for loops
+////        for (int i = 0; i < arr.length; i++) {
+////            arr[i] = in.nextInt();
+////            System.out.println(arr[i]);
+////        }
+//
+//        //array of objects
+//        String[] str = new String[4];
+//        for (int i = 0; i < str.length; i++) {
+//            arr[i] = in.next();
+//
+//        }
+//
+//        System.out.println(str);
+//    }
+//}
